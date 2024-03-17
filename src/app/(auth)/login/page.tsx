cc4ae0aab2875454aae80b6b56b5f7c1abb53d5c
G1-777 <println_g1@163.com>
2024-03-17 22:25:59 +0800

    fix login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,20 @@
 import { handleGithubLogin, login } from '@/lib/action';
 
+import styles from './login.module.css';
+
 const Login = async () => {
     return (
-        <div>
-            <form action={handleGithubLogin}>
-                <button>Login with Github</button>
-            </form>
-            <form action={login}>
-                <input type="text" name="username" placeholder="username" />
-                <input type="password" name="password" placeholder="password" />
-                <button>Login with Credentials</button>
-            </form>
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <form action={login} className={styles.credentials}>
+                    <input type="text" name="username" placeholder="username" />
+                    <input type="password" name="password" placeholder="password" />
+                    <button>Login with Credentials</button>
+                </form>
+                <form action={handleGithubLogin} className={styles.github}>
+                    <button>Login with Github</button>
+                </form>
+            </div>
         </div>
     );
 };
